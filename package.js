4eff53e758407171907a49799645a1fc22ea1257
Rodrigo <info@sapienlab.com>
2013-02-08 04:31:18 -0300

    package information

---
+++ package.js
@@ -0,0 +1,22 @@
+var miniExcludes = {
+		"sld/index.html": 1,
+		"sld/README.md": 1,
+		"sld/package": 1
+	},
+	isTestRe = /\/test\//;
+
+var profile = {
+	resourceTags: {
+		test: function(filename, mid){
+			return isTestRe.test(filename);
+		},
+
+		miniExclude: function(filename, mid){
+			return isTestRe.test(filename) || mid in miniExcludes;
+		},
+
+		amd: function(filename, mid){
+			return /\.js$/.test(filename);
+		}
+	}
+};
