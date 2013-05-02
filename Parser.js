44c1d620d905153a24581bbd9d31f9a06ee640bb
roro89 <rodrigo.gonzlez@gmail.com>
2013-05-02 01:49:22 -0400

    Fixed errors

--- Parser.js
+++ Parser.js
@@ -1,21 +1,60 @@
-define(['dojo/_base/declare', 'dojo/_base/lang', './parser'], function(declare, lang, parser) {
+define(['dojo/_base/declare', // declare 
+	'dojo/Stateful', // Stateful
+	'dojo/_base/lang', // lang 
+	'./parser' // parser
+	], function(declare, Stateful, lang, parser) {
 
-	return declare(null, {
+	return declare(Stateful, {
 
-		constructor : function(sld) {
-			this.sld = sld;
+		/**
+		 * 
+ 		 * @param [{Object}] args
+ 		 * @param [{Object}] args.sld
+ 		 * @param [{Array}] args.rules
+		 */
+		constructor : function(args) {
+			this._sld = args.sld || {};
+			this._rules = args.rules || [];
+			this._parsed = false;
+			this._counted = false;
+			this._dependences = false;
 		},
 
 		parse : function() {
-			return parser.parse(lang.clone(this.sld));
+			if (!this._parsed)
+				this._parsed = parser.parse(lang.clone(this._sld), this._rules);
+			return this._parsed;
 		},
 
 		count : function() {
-			return parser.count(this.sld);
+			if (this._counted === false)
+				this._counted = parser.count(this._sld);
+			return this._counted;
 		},
 
-		listDependences : function() {
-			return parser.listDependences(this.sld);
+		listDependences : function(toObject) {
+			if (!this._dependences)
+				this._dependences = parser.listDependences(this._sld, toObject);
+			return this._dependences;
+		},
+		
+		_sldGetter : function() {
+			return this._sld;
+		},
+
+		_sldSetter : function(sld) {
+			this._parsed = false;
+			this._counted = false;
+			this._dependences = false;
+			this._sld = sld;
+		},
+
+		_rulesGetter : function() {
+			return this._rules;
+		},
+
+		_rulesSetter : function(rules) {
+			this._rules = rules;
 		}
 	});
 
